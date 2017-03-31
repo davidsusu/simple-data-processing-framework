9ba5ce718b2718ee7f8b326e0bf46cff873ccd03
Horváth Dávid <horvath@webarticum.hu>
2017-03-31 04:13:01 +0200

    Added skeleton of ContentPanel and MainFrame

---
+++ solution-io-framework/src/hu/webarticum/siof/gui/MainFrame.java
@@ -0,0 +1,45 @@
+package hu.webarticum.siof.gui;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import hu.webarticum.siof.framework.TextSolution;
+
+public class MainFrame extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public MainFrame(
+        List<TextSolution> solutions,
+        File inputFile,
+        String inputContent,
+        File outputFile,
+        String outputContent,
+        File checkOutputFile,
+        String checkOutputContent
+    ) {
+        setSize(420, 300);
+        
+        // TODO
+        
+        TextSolution solution = solutions.get(0);
+        Reader reader = new StringReader(inputContent);
+        Writer writer = new StringWriter();
+        
+        try {
+            solution.solve(reader, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("Output:\n\n" + writer.toString());
+    }
+
+}
